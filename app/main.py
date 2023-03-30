d7509bd1da72030f6ee93e9f8a6483e47763dfe8
electrojam <electrojam@hotmail.com>
2023-03-30 03:18:58 +0000

    Media Poblacional de un país

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 import utils
 import read_csv
 import charts
-
+#media de la población de un país
 def run():
   data = read_csv.read_csv('./app/data.csv')
   country = input('Digite el país =>')
-  
+   
   result = utils.population_by_country(data, country)
-  
+   
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
